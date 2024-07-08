623ed58aad22ba8a348b7118f7c92c01013dacbe
Gabriell <21394475-cauagabriell201@users.noreply.replit.com>
2024-07-08 03:14:45 +0000

    PushBack implementado

--- src/lista.c
+++ src/lista.c
@@ -1,8 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void Lista_new(Lista* lista, int data_size, void (*free_data)(void*)) {
+void Lista_new(Lista *lista, int data_size, void (*free_data)(void *)) {
   lista->current = NULL;
   lista->head = NULL;
   lista->tail = NULL;
@@ -10,24 +10,38 @@ void Lista_new(Lista* lista, int data_size, void (*free_data)(void*)) {
   lista->free = free_data;
 }
 
-void Lista_delete(Lista* lista) {
-  Lista_new(lista, 0, lista->free);
-}
+void Lista_delete(Lista *lista) { Lista_new(lista, 0, lista->free); }
 
-int Lista_isEmpty(Lista* lista) {
-  if (lista->head == NULL) return 1;
+int Lista_isEmpty(Lista *lista) {
+  if (lista->head == NULL)
+    return 1;
   return 0;
 }
 
-int Lista_size(Lista* lista) {
-  return lista->size;
-}
+int Lista_size(Lista *lista) { return lista->size; }
 
-void Lista_pushFront(Lista* lista, void* valor) {
-  ListaNodo *new = (ListaNodo*)malloc(sizeof(ListaNodo));
+void Lista_pushFront(Lista *lista, void *valor) {
+  ListaNodo *new = (ListaNodo *)malloc(sizeof(ListaNodo));
   new->valor = valor;
   new->next = lista->head;
-  if (lista->head == NULL) lista->tail = new;
+  if (lista->head == NULL)
+    lista->tail = new;
   lista->head = new;
+  lista->size++;
+}
+
+void Lista_pushBack(Lista *lista, void *valor) {
+  ListaNodo *new = (ListaNodo *)malloc(sizeof(ListaNodo));
+  new->valor = valor;
+  new->next = NULL;
+
+  if (lista->head == NULL) {
+    lista->head = new;
+    lista->tail = new;
+  } else {
+    lista->tail->next = new;
+    lista->tail = new;
+  }
+
   lista->size++;
 }
